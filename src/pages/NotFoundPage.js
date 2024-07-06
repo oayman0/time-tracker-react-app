1815190eaf8dd6b044da2324c85a0423f2a437f3
oayman0 <omaraymankhalaf@gmail.com>
2024-07-07 01:32:29 +0300

    redirection

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,11 +1,13 @@
 import Footer from "../components/Footer";
 import LandingNavBar from "../components/LandingNavBar";
+import SimpleNavBar from "../components/SimpleNavBar";
 import NotFound from "../components/NotFound";
 
 function NotFoundPage (){
     return(
         <>
-        <LandingNavBar/>
+        {/* <LandingNavBar/> */}
+        <SimpleNavBar/>
         <NotFound/>
         <Footer/>
         </>
